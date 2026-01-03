09e7b5a642572fb36068ea89a409491fabb27937
Sarahw54545 <20102911@mail.wit.ie>
2026-01-03 22:57:49 +0000

    Add backend API + database connection

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helloRouter from "./api/hello/index.js";
+import {pool} from "./db.js"
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+pool.query("SELECT NOW()")
+.then(() => console.log("PostgreSQL Connected"))
+.catch(err => console.error("PostgreSQL connection error", err));
+
+app.use("/api", helloRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.info(`Server running on port ${PORT}`);
+});
