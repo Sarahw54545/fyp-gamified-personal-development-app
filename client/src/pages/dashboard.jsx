b2dd01dab0b393cde86c48513270ef0c10fd3009
Sarahw54545 <20102911@mail.wit.ie>
2026-02-09 20:34:08 +0000

    Fix case sensitivity issue with imports

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,9 +1,9 @@
 import MainLayout from "../components/layout/mainLayout";
-import { WelcomeHeader } from "../components/dashboard/WelcomeHeader";
-import { XPWidget } from "../components/dashboard/XPWidget";
-import { GoalPreview } from "../components/dashboard/GoalPreview";
-import { StreakCard } from "../components/dashboard/StreakCard";
-import { AchievementPreview } from "../components/dashboard/AchievementPreview";
+import { WelcomeHeader } from "../components/dashboard/welcomeHeader";
+import { XPWidget } from "../components/dashboard/xpWidget";
+import { GoalPreview } from "../components/dashboard/goalPreview";
+import { StreakCard } from "../components/dashboard/streakCard";
+import { AchievementPreview } from "../components/dashboard/achievementPreview";
 
 function Dashboard() {
   return (
